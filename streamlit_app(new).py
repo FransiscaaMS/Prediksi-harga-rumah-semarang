421bc03467bdaa050de6bd3c9f19d108bba1c491
Fransisca Mulya Sari <128776177+FransiscaaMS@users.noreply.github.com>
2024-05-30 08:52:02 +0700

    Update streamlit_app(new).py

--- streamlit_app(new).py
+++ streamlit_app(new).py
@@ -104,7 +104,7 @@ if test == "Prediksi":
     with col1:
         Jenis_Rumah = st.selectbox("Jenis Rumah", [0, 1, 2])
     with col2:
-        st.selectbox("Lokasi", [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14, 15, 16])
+        Lokasi = st.selectbox("Lokasi", [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14, 15, 16])
     with col1:
         KT = st.number_input('Input Jumlah Kamar Tidur', min_value=1, max_value=10)
     with col2:
